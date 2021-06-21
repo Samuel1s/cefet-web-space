776f236e62ff50ecacda2872685a72a62ba7cf4a
Samuel Santos <Samuel@MacBook-Pro-de-Samuel-Santos.local>
2021-06-21 19:55:07 -0300

    Término do TP.

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,16 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
+
+const massa1El = document.querySelector('#massa1');
+const massa2El = document.querySelector('#massa2');
+const gravidadeEl = document.querySelector('#constante');
+const distanciaEl = document.querySelector('#distancia');
+const resultadoEl = document.querySelector("#calcular");
+
+resultadoEl.addEventListener('click', function() {   
+    const forca = (gravidadeEl.value * massa1El.value * massa2El.value)/(distanciaEl.value * distanciaEl.value);
+
+    document.querySelector('#resultado').value = forca;  
+});
+
+
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,19 @@
 // Faça o exercício dos PARÁGRAFOS aqui
+const botoes = document.querySelectorAll(".botao-expandir-retrair");
+
+for (botaoEl of botoes) {
+    botaoEl.addEventListener('click', erParagrafos);
+}
+
+function erParagrafos(e) {
+    if (e.currentTarget.innerHTML == '+') {
+        e.currentTarget.innerHTML = '-';
+
+    } else {
+        e.currentTarget.innerHTML = '+';
+
+    }
+    e.currentTarget.parentNode.classList.toggle('expandido');
+}
+
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
